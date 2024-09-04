7109011e0f24846aa11137caf99a315448b60c84
Patrizio Tardiolo Bonifazi <p.tardiolobonifazi@vivasoft.it>
2024-09-04 11:04:25 +0200

    login typescript

---
+++ src/Identity/Logout/Logout.tsx
@@ -0,0 +1,18 @@
+import { FC, ReactElement, ReactNode, useState } from 'react';
+import styles from './Logout.module.css';
+
+// import { LoginData } from '../../Models/logindata';
+
+
+interface LogoutProps {};
+
+const Logout: FC<LogoutProps> = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default Logout;
